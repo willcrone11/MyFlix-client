7eea27d53fe221211fb8b1893b1a8e4fdb0785ae
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-01-15 21:10:13 -0500

    Creates MainView component which loads data from star wars api and displays list of movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,19 +5,54 @@ import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+export class MainView extends React.Component {
+
   constructor() {
     // Call the superclass constructor so React can initialize it
     super();
 
     // Initialize the state to an empty object so we can destructure it later
-    this.state = {};
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
   }
 
-  // This overrides the render() method of the superclass
-  // No need to call super() though, as it does nothing by default
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios.get('https://starwarscentral.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
   render() {
+    const { movies, selectedMovie } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view"/>;
+
     return (
-      <div className="main-view"></div>
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie={selectedMovie}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+          ))
+        }
+      </div>
     );
   }
 }
