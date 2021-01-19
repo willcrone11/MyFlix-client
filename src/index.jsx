18f14a266463c5c2af49f5a5f60e008a70562b26
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-01-19 15:03:17 -0500

    Adds container that holds the entire contents of the app

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
 
 import { MainView } from './components/main-view/main-view';
 
@@ -9,7 +10,11 @@ import './index.scss';
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView/>;
+    return (
+      <Container>
+        <MainView />
+      </Container>
+    );
   }
 }
 
