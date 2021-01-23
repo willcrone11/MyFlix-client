ed0002a869fd78e18655d99cf025565643ebeae1
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-01-22 22:44:10 -0500

    Eliminates unnecessary code and modifies existing code to work more smoothly with app

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from "react-router-dom";
+
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
@@ -10,15 +12,17 @@ export class MovieCard extends React.Component {
     // This is given to the <MovieCard/> component by the outer world
     // which, in this case, is `MainView`, as `MainView` is what’s
     // connected to your database via the movies endpoint of your API
-    const { movie, onClick } = this.props;
+    const { movie } = this.props;
 
     return (
-      <Card style={{ width: '16rem', height: '60rem' }}>
+      <Card style={{ width: '16rem', height: '60rem' }} className="movie-card">
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onClick(movie)} variant="link">Open</Button>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link">Open</Button>
+          </Link>
         </Card.Body>
       </Card>
     );
@@ -41,5 +45,4 @@ MovieCard.propTypes = {
       Death: PropTypes.string,
     }),
   }).isRequired,
-  onClick: PropTypes.func.isRequired,
 };
