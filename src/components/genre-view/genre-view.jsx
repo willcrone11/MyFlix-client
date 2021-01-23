369a8871dd3e4108e4313d2365fb425087eea37c
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-01-22 22:42:34 -0500

    Adds a genre view component where genre info can be viewed

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+import './genre-view.scss';
+
+
+export class GenreView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    const { genre } = this.props;
+
+    return (
+      <div className="genre-view">
+        <div className="genre-name">
+          <span className="label">Name: </span>
+          <span className="value">{genre.Name}</span>
+        </div>
+        <br />
+        <div className="genre-description">
+          <span className="label">Description: </span>
+          <span className="value">{genre.Description}</span>
+        </div>
+        <br />
+        <Link to={'/'}>
+          <Button variant="primary">Back</Button>
+        </Link>
+      </div>
+    );
+  }
+}
