ad0354aebfd3adec9ba822679d1bf09bb00c9efa
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-02-02 13:26:53 -0500

    Adds container for login view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -29,37 +29,38 @@ export function LoginView(props) {
 
   return (
     <React.Fragment>
-      <Form className='form-login'>
-        <h1 className='text-primary'>Welcome to myFlix!</h1>
-        <p className='mb-5'>Please login to continue.</p>
-        <Form.Group controlId='formBasicEmail'>
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type='text'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder='Enter Username'
-          />
-        </Form.Group>
-        <Form.Group controlId='formBasicPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type='password'
-            placeholder='Enter Password'
-          />
-        </Form.Group>
-        <Button onClick={handleSubmit} variant='primary' type='submit'>
-          Submit
-        </Button>
-      </Form>
-      <div className="no-account">
-        <span>Don't Have an Account?</span>
-          <Link to={`/register`}>
-            <Button variant="link">Register Here</Button>
-          </Link>
+      <div className="container">
+        <Form className='form-login'>
+          <p className='mb-5'>Please login to continue.</p>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder='Enter Username'
+            />
+          </Form.Group>
+          <Form.Group controlId='formBasicPassword'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type='password'
+              placeholder='Enter Password'
+            />
+          </Form.Group>
+          <Button onClick={handleSubmit} variant='primary' type='submit'>
+            Submit
+          </Button>
+        </Form>
+        <div className="no-account">
+          <span>Don't Have an Account?</span><br/>
+            <Link to={`/register`}>
+              <Button variant="link">Register Here</Button>
+            </Link>
+        </div>
       </div>
     </React.Fragment>
   );
