e0d062aaa2f83f0761af3df77d5859eb5fb37368
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-02-02 13:23:34 -0500

    Creates store for redux funtionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import Container from 'react-bootstrap/Container';
 
-import { MainView } from './components/main-view/main-view';
+import MainView from './components/main-view/main-view';
+import moviesApp from './reducers/reducers';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container className="container">
+          <Row className="justify-content-md-center">
+            <Col>
+              <MainView />
+            </Col>
+          </Row>
+        </Container>
+      </Provider>
     );
   }
 }
