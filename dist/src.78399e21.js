5ef792c7e97aa323e4d9255740147caccbea2c6c
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-02-11 23:52:23 -0500

    Builds app for dist via parcel

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -55114,7 +55114,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50162" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51164" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
